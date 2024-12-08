ef7d3dd47e11a779dd9a5317a0d4559ca20a88bb
Luiz Inaldo <luiz.inaldo970@gmail.com>
2024-12-07 21:41:03 -0300

    :star: feat: adiciona filtro da lista de históricos
    # O que foi feito nessa feature
    
    - Componentes de layout (exceto o principal) sofreram ligeiras mudanças. Foram removidos os imports de fonte, assim como a renderização de um novo body a cada chamada desse layout, que estava causando um erro de hydration.
    
    - Página de histórico recebeu o filtro por mês e ano.
    
    - Componente de Header agora é dinâmico em questão de conteúdo. Mas, diferente da abordagem comum de renderizar o children, ele recebe como prop uma função que renderiza um ReactNode, e que também recebe argumentos, passando um state (ou mais, se necessários futuramente) do header para o content.
    
    - Componente Main e Histórico recebe a nova abordagem do componente Header.
    
    - Inserido novo tipo do doc de types.

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -1,9 +1,4 @@
-import { Quicksand } from "next/font/google";
-import "../../../styles/globals.css";
 import { Metadata } from "next";
-import VerifyDevice from "@/components/VerifyDevice";
-
-const quicksand = Quicksand({ weight: ['300', '400', '500', '700'], subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Faça login e começe | Minha Lista",
@@ -22,14 +17,8 @@ export default function RootLayout({
 }>) {
 
     return (
-        <html lang="pt-br">
-            <body className={quicksand.className}>
-                <div className="relative">
-                    <VerifyDevice>
-                        {children}
-                    </VerifyDevice>
-                </div>
-            </body>
-        </html>
+        <>
+        {children}
+        </>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,10 @@
 "use client";
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { ChevronRight, SlidersHorizontal } from 'lucide-react'
-import { ProductsContext } from '@/context/ProductsContext';
-import Link from 'next/link';
-import { APP_ROUTES } from '@/routes/app-routes';
+import React, { useEffect, useRef, useState } from 'react'
 
-const Header = () => {
+const Header = ({ content }: {
+    content: (_: any, visible?:boolean) => React.ReactNode;
+}) => {
 
-    const { user } = useContext(ProductsContext);
     const [isIconVisible, setIsIconVisible] = useState<boolean>(true);
     const headerRef = useRef<HTMLHeadElement | null>(null);
 
@@ -37,22 +33,7 @@ const Header = () => {
 
     return (
         <header ref={headerRef} className="fixed top-0 left-0 z-[3] w-full p-4 bg-secondary-dark flex items-center justify-between transition-shadow duration-500">
-            <div className='flex items-center gap-3 cursor-pointer overflow-hidden'>
-                <Avatar className='border-2 border-snow'>
-                    <AvatarImage src="images/profile.JPG" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className='flex items-center'>
-                    <p className={`${isIconVisible ? "max-w-[89px] mr-1" : "max-w-0"} overflow-hidden whitespace-nowrap text-titledark transition-all duration-200`}>
-                        Bem-vindo,
-                    </p>
-                    <p className='text-titledark'>{user?.user_metadata?.name || 'Usuário sem nome.'}</p>
-                    <ChevronRight size={16} className={`${isIconVisible ? "opacity-100 translate-x-0 ml-2" : "opacity-0 -translate-x-full"} transition-all duration-200 text-titledark`} />
-                </div>
-            </div>
-            <Link href={APP_ROUTES.private.settings.name}>
-                <SlidersHorizontal size={20} className='cursor-pointer text-titledark' />
-            </Link>
+            {content(this, isIconVisible)}
         </header>
     )
 }
