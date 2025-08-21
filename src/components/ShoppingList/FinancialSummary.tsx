6e3e8bbe52307dee2314d7482774c04d06fa7970
Luiz Inaldo <luiz.inaldo970@gmail.com>
2025-08-21 15:58:11 -0300

    :sparkles: feat: Adiciona barra de progresso circular animada e atualiza layout do resumo financeiro

--- src/components/ShoppingList/FinancialSummary.tsx
+++ src/components/ShoppingList/FinancialSummary.tsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { useShoplistContext } from "@/context/ShoplistContext";
 import { formatCurrency } from "@/functions/formatCurrency";
 import { useCallback, useEffect } from "react";
+import { AnimatedCircularProgressBar } from "../magicui/animated-circular-progress-bar";
 
 export function FinancialSummary() {
 
@@ -14,14 +15,14 @@ export function FinancialSummary() {
         let progress: number;
 
         if (!auxData) return 0;
-        
+
         const totalCurrentListValue = totalValue;
         const totalMaxValue = auxData.max_value;
-        
+
         progress = Math.min(100, (totalCurrentListValue / totalMaxValue) * 100);
 
         return progress;
-        
+
     }, [auxData, totalValue]);
 
     return (
@@ -30,17 +31,30 @@ export function FinancialSummary() {
                 <TrendingUp size={20} className="text-green-600 dark:text-green-400" />
                 <h3 className="text-sm text-subtitle">Resumo financeiro</h3>
             </div>
-            <div className="flex items-center">
-                <div className="flex-1 text-subtitle text-center">
-                    <p className="text-sm">Itens</p>
-                    <p className="text-lg font-semibold">{auxData?.purchase_items?.length || 0}</p>
+            <div className="flex items-center py-3 gap-8 border-b border-app-border">
+                <div className="flex items-center justify-center pl-4">
+                    <AnimatedCircularProgressBar
+                        min={0}
+                        max={100}
+                        value={calculateTotalProgress()}
+                    />
                 </div>
-                <div className="flex-1 text-center">
-                    <p className="text-sm text-subtitle">Valor atual</p>
-                    <p className="text-lg font-semibold text-app-primary">{formatCurrency(totalValue || 0)}</p>
+                <div className="shrink-0 flex-1 grid gap-1 items-center">
+                    <div className="text-subtitle ">
+                        <p className="text-sm">Itens</p>
+                        <p className="text-lg font-semibold">{auxData?.purchase_items?.length || 0}</p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-subtitle">Valor atual</p>
+                        <p className="text-lg font-semibold text-default-green">{formatCurrency(totalValue || 0)}</p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-subtitle">Orçamento</p>
+                        <p className="text-lg font-semibold text-action">{formatCurrency(auxData?.max_value || 0)}</p>
+                    </div>
                 </div>
             </div>
-            <div className="space-y-1">
+            {/* <div className="space-y-1">
                 <div className="flex items-center justify-between text-xs text-paragraph">
                     <p>orçamento estabelecido</p>
                     <p className="italic">
@@ -48,7 +62,7 @@ export function FinancialSummary() {
                     </p>
                 </div>
                 <Progress value={calculateTotalProgress()} />
-            </div>
+            </div> */}
             <Button className="w-full">
                 <ShoppingCart />
                 <p>Finalizar Compra</p>
